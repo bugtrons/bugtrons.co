94c64e622b3643da22cb5d076d02c50ccaf7702f
Ghulam Yazdani <ghulamyazdani12@gmail.com>
2021-12-09 21:31:09 +0530

    Navbar added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import { About } from "./Components/About/About";
+import Navbar from "./Components/Navbar/Navbar";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 
 import "./App.scss";
 function App() {
   return (
-    <div className="App">
-      <About />;
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        {/* <About /> */}
+      </div>
+    </Router>
   );
 }
 
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,5 +1,56 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
+import Logo from "./logo.png";
+import "./Navbar.scss";
 export default function Navbar() {
-  return <div>Hello</div>;
+  return (
+    <header id="header" className="header fixed-top">
+      <div className="container-fluid container-xl d-flex align-items-center justify-content-between">
+        <Link to="index.html" className="logo d-flex align-items-center">
+          <img src={Logo} alt="Logo" />
+          <span>BUGTRONS</span>
+        </Link>
+
+        <nav id="navbar" className="navbar">
+          <ul>
+            <li>
+              <a className="nav-link scrollto active" href="#hero">
+                Home
+              </a>
+            </li>
+            <li>
+              <a className="nav-link scrollto" href="#about">
+                About
+              </a>
+            </li>
+            <li>
+              <a className="nav-link scrollto" href="#services">
+                Schedule
+              </a>
+            </li>
+            <li>
+              <a className="nav-link scrollto" href="#portfolio">
+                Leaderboard
+              </a>
+            </li>
+            <li>
+              <a className="nav-link scrollto" href="#team">
+                Swags
+              </a>
+            </li>
+            <li>
+              <a href="blog.html">Sponsors</a>
+            </li>
+
+            <li>
+              <Link to="/contact" className="nav-link scrollto">
+                Register
+              </Link>
+            </li>
+          </ul>
+          <i className="bi bi-list mobile-nav-toggle"></i>
+        </nav>
+      </div>
+    </header>
+  );
 }
