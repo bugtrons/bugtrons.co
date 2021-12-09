6c74272cc6befe47d5c28222dc759597ae6b9ef6
dhruvsol <dhruvrajsinghsolanki161@gmail.com>
2021-12-09 12:40:28 +0530

    make the about section

---
+++ src/Components/About/About.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./About.scss";
+export const About = () => {
+  return (
+    <div className="about">
+      <h3 className="about-head">About Bugtrons 2.0 </h3>
+      <div className="about-info">
+        <p>
+          The
+          <strong> BUGTRONS 2.0</strong>
+          , revamped version of BUGTRONS, is a student-run contest, organized
+          <br />
+          with the collaboration of two tremendous tech-lead communities:-
+        </p>
+        <p>
+          DEVSTRONS': Student-run Open source/ project-based, inclusive &
+          diverse community
+          <br />
+          intending to Learn, Code and Tron (grow).
+        </p>
+        <p>
+          Learn With Community: Let's Learn, grow and give back to this
+          Community.
+        </p>
+      </div>
+    </div>
+  );
+};
