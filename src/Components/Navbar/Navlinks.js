57de3e5a57bbbe1709716216a1106342d6c0f5a4
Ghulam Yazdani <ghulamyazdani12@gmail.com>
2021-12-11 12:57:22 +0530

    new

--- src/Components/Navbar/Navlinks.js
+++ src/Components/Navbar/Navlinks.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export default function Navlinks() {
   return (
-    <ul id="navlinks" className="showNavbar show">
+    <ul id="navlinks" className="navsh">
       <li>
         <a className="nav-link scrollto active" href="#hero">
           Home
