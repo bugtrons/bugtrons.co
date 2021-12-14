1ba29700144267e9e6dcb6db88cbcf327788d272
dhruvsol <dhruvrajsinghsolanki161@gmail.com>
2021-12-14 19:51:02 +0530

    sponsor sections done

---
+++ src/Components/Sponsors/sponsorClass/sponsor.class.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './sponsor.class.scss'
+export const SponsorClass = ({sclass}) => {
+    return (
+        <div className='sponsorClass'>
+           <h1>{sclass}</h1>
+        </div>
+    )
+}
