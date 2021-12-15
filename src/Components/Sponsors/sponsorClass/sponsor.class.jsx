3bcaa7a287603823753d3d87d51a28885126fb1b
akshatnema <20bcs022@iiitdmj.ac.in>
2021-12-15 11:47:02 +0530

    Revert "sponsor sections done"
    This reverts commit 1ba29700144267e9e6dcb6db88cbcf327788d272.

--- src/Components/Sponsors/sponsorClass/sponsor.class.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import './sponsor.class.scss'
-export const SponsorClass = ({sclass}) => {
-    return (
-        <div className='sponsorClass'>
-           <h1>{sclass}</h1>
-        </div>
-    )
-}
--- src/Components/Sponsors/sponsorLogo/sponsor.logo.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import './sponsor.Logo.scss'
-export const SponsorLogo = ({image}) => {
-    return (
-        <div className='SponsorLogo'>
-            <img src={image} alt="sponsor Logo" />
-        </div>
-    )
-}
