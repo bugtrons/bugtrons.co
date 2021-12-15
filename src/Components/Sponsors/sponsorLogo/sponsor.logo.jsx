de2133d344587a1e75b96390881c5dc958efe3cf
dhruvsol <dhruvrajsinghsolanki161@gmail.com>
2021-12-15 14:42:12 +0530

    make changes in size of the labels

--- src/Components/Sponsors/sponsorLogo/sponsor.logo.jsx
+++ src/Components/Sponsors/sponsorLogo/sponsor.logo.jsx
@@ -3,7 +3,7 @@ import './sponsor.Logo.scss'
 export const SponsorLogo = ({image}) => {
     return (
         <div className='SponsorLogo'>
-            <img src={image} alt="sponsor Logo" />
+            <img  src={image} alt="sponsor Logo"  />
         </div>
     )
 }
