3060083d6fc5efda308a6c3f0b045ddb67b08c6d
Krish Gupta <krishguptadev@outlook.com>
2021-12-17 05:17:22 +0000

    feat: react-tabs

--- src/Components/Schedule/Schedule.js
+++ src/Components/Schedule/Schedule.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+
 import "./Schedule.scss";
 
 export default function Schedule() {
   return (
     <div
+      className="schedule-container"
       data-aos="fade-up"
       data-aos-offset="100"
       data-aos-delay="50"
@@ -11,56 +14,37 @@ export default function Schedule() {
       data-aos-easing="ease-in-out"
       data-aos-mirror="true"
     >
-      <div className="schedule">
-        <h3 className="schedule-head">Schedule</h3>
-        <table className="table table-hover table-bordered align-middle">
-          <tbody>
-            <tr>
-              <td rowspan="4" className="first">
-                Day 1
-              </td>
-            </tr>
-            <tr>
-              <td>Opening Ceremony</td>
-            </tr>
-            <tr>
-              <td>Round 1 - DSA Competition</td>
-            </tr>
+      <h2 className="schedule-heading">Schedule</h2>
+      <Tabs>
+        <TabList>
+          <Tab>Day 1</Tab>
+          <Tab>Day 2</Tab>
+          <Tab>Day 3</Tab>
+        </TabList>
 
-            <tr>
-              <td>Announcement of top 20 winners</td>
-            </tr>
-
-            <tr>
-              <td rowspan="4" className="first">
-                Day 2
-              </td>
-            </tr>
-
-            <tr>
-              <td>Round 2 - Debugging Contest</td>
-            </tr>
-
-            <tr>
-              <td>Announcement of top 10 winners</td>
-            </tr>
-            <tr>
-              <td>XYZ Session</td>
-            </tr>
-            <tr>
-              <td rowspan="4" className="first">
-                Day 3
-              </td>
-            </tr>
-            <tr>
-              <td>Final Round - DSA Competition</td>
-            </tr>
-            <tr>
-              <td>Announcement of top 3 winners and Closing Ceremony</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+        <TabPanel className="table">
+          <div className="row-set">
+            <div className="row">
+              <div className="col session-name">Opening Ceremony</div>
+              <div className="col session-time">6:00pm IST</div>
+            </div>
+            <div className="row">
+              <div className="col session-name">Opening Ceremony</div>
+              <div className="col session-time">6:00pm IST</div>
+            </div>
+            <div className="row">
+              <div className="col session-name">Opening Ceremony</div>
+              <div className="col session-time">6:00pm IST</div>
+            </div>
+          </div>
+        </TabPanel>
+        <TabPanel>
+          <h2>Any content 2</h2>
+        </TabPanel>
+        <TabPanel>
+          <h2>Any content 3</h2>
+        </TabPanel>
+      </Tabs>
     </div>
   );
 }
