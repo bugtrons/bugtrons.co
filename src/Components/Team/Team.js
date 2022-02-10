3b43a48ab7a718e0e6ff397939b5b3310c398fda
amino19 <anshumaankrprasad76@gmail.com>
2022-02-10 15:49:46 +0530

    ✨ | Enhancing Team page

--- src/Components/Team/Team.js
+++ src/Components/Team/Team.js
@@ -18,7 +18,7 @@ function Team() {
         <Card
           name="Ghulam Yazdani"
           img="https://pbs.twimg.com/profile_images/1460342339310788614/96gkpqcQ_400x400.jpg"
-          position="Organizer"
+          position="Organizer & Developer"
           github="https://github.com/ghulamyazdani"
           linkedin="https://www.linkedin.com/in/ghulamyazdani"
           gmail="mailto:ghulamyazdani12@gmail.com"
@@ -27,16 +27,25 @@ function Team() {
         <Card
           name="Anshumaan Kr. Prasad"
           img="https://avatars.githubusercontent.com/amino19"
-          position="Organizer"
+          position="Organizer & Outreach"
           github="https://github.com/amino19"
           linkedin="https://www.linkedin.com/in/amino19"
           gmail="mailto:anshumaankrprasad76@gmail.com"
           twitter="https://twitter.com/aminostwt"
         />
         <Card
+          name="Payal Samal"
+          img="https://cdn.discordapp.com/attachments/922140511328927834/941255473519276032/IMG_20220210_142301.jpg"
+          position="Operations - Tester"
+          github="https://github.com/Payal39"
+          linkedin="https://www.linkedin.com/in/payal-samal-a08971227/"
+          gmail="mailto:samalpayal03@gmail.com"
+          twitter="https://twitter.com/payal_samal"
+        />
+        <Card
           name="Aditya Sahu"
           img="https://avatars.githubusercontent.com/adityasahu252"
-          position="Organizer"
+          position="Organizer & Community Manager"
           twitter=" https://twitter.com/AdityaSahuDev"
           github="https://github.com/adityasahu252"
           linkedin="https://www.linkedin.com/in/aditya-sahu-477a69175/"
@@ -45,34 +54,43 @@ function Team() {
         <Card
           name="Akshat Nema"
           img="https://avatars.githubusercontent.com/akshatnema"
-          position="Organizer"
+          position="Problems Setter & Developer"
           twitter="https://twitter.com/AksNema"
           github="https://github.com/akshatnema"
           linkedin="https://www.linkedin.com/in/akshat-nema-2002/"
           gmail="mailto:akshatnema.school@gmail.com"
         />
         <Card
-          name="Joshith Reddy Gopidi"
-          img="https://pbs.twimg.com/profile_images/1470063414189776900/dA9kKfVD_400x400.jpg"
-          position="Organizer"
-          twitter="https://twitter.com/JoshithReddy_g"
-          github="https://github.com/JOS-RE"
-          linkedin="https://www.linkedin.com/in/joshith-reddy-gopidi/"
-          gmail="mailto:joshith.gopidi@gmail.com"
+          name="Abir Pal"
+          img="https://pbs.twimg.com/profile_images/1440399129448890381/MWeteGwW_400x400.jpg"
+          position="MVP & Speaker Support"
+          github="https://github.com/imabp/"
+          twitter="https://twitter.com/imabptweets/"
+          linkedin="https://www.linkedin.com/in/imabp/"
+          gmail="mailto:abir.pal899@gmail.com"
         />
         <Card
           name="Priyanka Prasad"
           img="https://avatars.githubusercontent.com/prykdev"
-          position="Organizer"
+          position="Outreach & Community Manager"
           twitter="https://twitter.com/prykdev"
           github="https://github.com/prykdev"
           linkedin="https://www.linkedin.com/in/prykdev/"
           gmail="mailto:developer@priyankaprasad.co"
         />
         <Card
+          name="Joshith Reddy Gopidi"
+          img="https://pbs.twimg.com/profile_images/1470063414189776900/dA9kKfVD_400x400.jpg"
+          position="UI/UX Designer"
+          twitter="https://twitter.com/JoshithReddy_g"
+          github="https://github.com/JOS-RE"
+          linkedin="https://www.linkedin.com/in/joshith-reddy-gopidi/"
+          gmail="mailto:joshith.gopidi@gmail.com"
+        />
+        <Card
           name="Harsha Sumanchandra"
           img="https://avatars.githubusercontent.com/Harsha200105"
-          position="Organizer"
+          position="Content Creator"
           github="https://github.com/Harsha200105"
           twitter="https://twitter.com/Harsha052001"
           linkedin="https://www.linkedin.com/in/harsha-sumanchandra/"
@@ -81,7 +99,7 @@ function Team() {
         <Card
           name="Ashish Chawda"
           img="https://avatars.githubusercontent.com/pixan198"
-          position="Organizer"
+          position="Outreach & GitHub Support"
           github="https://github.com/pixan198/"
           twitter="https://twitter.com/ashish11chawda/"
           linkedin="https://www.linkedin.com/in/ashishchawda/"
@@ -91,10 +109,5 @@ function Team() {
     </div>
   );
 }
-
-// ! 1st - Joshith, Ghulam bhaiya, Aminos, Akshat
-// * 2nd - Harsha, Priyanka, Payal
-// ? 3rd - Ashish bhaiya, Abir bhaiya
-// & 4th - Aditya bhaiya
 
 export default Team;
