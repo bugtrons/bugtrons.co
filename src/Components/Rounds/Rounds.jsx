0c6833e8c511e3a999547528d850b0e199dd6158
akshatnema <20bcs022@iiitdmj.ac.in>
2022-03-04 15:29:17 +0530

    code formatted
    Signed-off-by: akshatnema <20bcs022@iiitdmj.ac.in>

--- src/Components/Rounds/Rounds.jsx
+++ src/Components/Rounds/Rounds.jsx
@@ -4,22 +4,39 @@ export default function Rounds() {
   return (
     <div className="Rounds">
       <div className="Rounds-head">Rounds</div>
-      <div class="alig">
+      <div className="alig">
         <div className="Round">
           <h1>Problem Solving</h1>
           <p>This round will be on Hackerank</p>
-          <a href="https://github.com/bugtrons/bugtrons-2/blob/main/instructions.md"><span>More Details </span></a>
+          <a
+            href="https://www.hackerrank.com/bugtrons"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <span>More Details </span>
+          </a>
         </div>
         <div className="Round">
           <h1>Debugging Round </h1>
-          <p>This round will be on 
-Github and Replit</p>
-          <a href="https://github.com/bugtrons/bugtrons-2/blob/main/instructions.md"><span>More Details </span></a>
+          <p>This round will be on Github and Replit</p>
+          <a
+            href="https://github.com/bugtrons/bugtrons-2/blob/main/instructions.md"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <span>More Details </span>
+          </a>
         </div>
         <div className="Round">
           <h1>Competitive Programming Round </h1>
-          <p>This round will also be on  be on Hackerank</p>
-          <a href="https://github.com/bugtrons/bugtrons-2/blob/main/instructions.md"><span>More Details </span></a>
+          <p>This round will also be on be on Hackerank</p>
+          <a
+            href="https://github.com/bugtrons/bugtrons-2/blob/main/instructions.md"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <span>More Details </span>
+          </a>
         </div>
       </div>
     </div>
