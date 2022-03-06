9c6f97a8ef5a68aaa8979fea3b53c21bb734b16b
akshatnema <20bcs022@iiitdmj.ac.in>
2022-03-06 14:10:08 +0530

    Top 10 added

--- src/Components/Leaderboards/Top10/Data.js
+++ src/Components/Leaderboards/Top10/Data.js
@@ -1,13 +1,13 @@
 const Data = [
-  { Rank: 1, name: "Partha Sarathi Bhunia", score: "2410.00" },
-  { Rank: 2, name: "Rishabh Raizada", score: "2407.00" },
-  { Rank: 3, name: "Ishaan Gupta", score: "2100.00" },
-  { Rank: 4, name: "Rishabh Raizada", score: "2100.00" },
-  { Rank: 5, name: "Ash-KODES ", score: "2100.00" },
-  { Rank: 6, name: "Ishaan Gupta", score: "2100.00" },
-  { Rank: 7, name: "Mihirkumar Patel ", score: "1952.63" },
-  { Rank: 8, name: "Prakash A", score: "1900.00" },
-  { Rank: 9, name: "Ashish Pandagre", score: "1800.00" },
-  { Rank: 10, name: "Bharath Kumar kallepalli", score: "1661.33" },
+  { Rank: 1, name: "Partha Sarathi Bhunia", score: "300.00" },
+  { Rank: 2, name: "Rishabh Raizada", score: "300.00" },
+  { Rank: 3, name: "Ishaan Gupta", score: "250.00" },
+  { Rank: 4, name: "Ash-KODES", score: "216.67" },
+  { Rank: 5, name: "Prakash A", score: "200.00" },
+  { Rank: 6, name: "Mihirkumar Patel", score: "200.00" },
+  { Rank: 7, name: "Ayush Kalia", score: "163.89" },
+  { Rank: 8, name: "Soham Chaudhary", score: "159.96" },
+  { Rank: 9, name: "Varun Chawla", score: "155.56" },
+  { Rank: 10, name: "Ashish Pandagree", score: "150.00" },
 ];
 export default Data;
--- src/Components/Leaderboards/Top10/Top10.jsx
+++ src/Components/Leaderboards/Top10/Top10.jsx
@@ -5,17 +5,17 @@ export default function Top10() {
     <>
       <div className="Top-head">Top 10 Hackers</div>
       <div className="Lead-card">
-        <div className="high">
-          <p>Rank</p>
-          <p className="name">Name</p>
-          <p>Score</p>
+        <div className="row high">
+          <p className="col-3">Rank</p>
+          <p className="col-6 name">Name</p>
+          <p className="col-3">Score</p>
         </div>
         {Data.map((data, index) => {
           return (
-            <div className="part" key={index}>
-              <p>{data.Rank}</p>
-              <p className="name">{data.name}</p>
-              <p>{data.score}</p>
+            <div className="row part" key={index}>
+              <p className="col-3">{data.Rank}</p>
+              <p className="col-6 name">{data.name}</p>
+              <p className="col-3">{data.score}</p>
             </div>
           );
         })}
