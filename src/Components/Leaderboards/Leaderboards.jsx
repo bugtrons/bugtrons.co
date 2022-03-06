436aa7df42a7c35e824e63c9346da6974990776a
Ghulam Yazdani <ghulamyazdani12@gmail.com>
2022-03-06 13:55:11 +0530

    added To 10

--- src/Components/Leaderboards/Leaderboards.jsx
+++ src/Components/Leaderboards/Leaderboards.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Top20 from './Top20/Top20'
+import Top10 from './Top10/Top10'
 import './Leaderboard.scss'
 export default function Leaderboards() {
   return (
     <div className="leader">
     <div>Leaderboards</div>
+    <Top10 />
     <Top20 />
     </div>
   )
---
+++ src/Components/Leaderboards/Top10/Data.js
@@ -0,0 +1,13 @@
+const Data = [
+  { Rank: 1, name: "Partha Sarathi Bhunia", score: "2410.00" },
+  { Rank: 2, name: "Rishabh Raizada", score: "2407.00" },
+  { Rank: 3, name: "Ishaan Gupta", score: "2100.00" },
+  { Rank: 4, name: "Rishabh Raizada", score: "2100.00" },
+  { Rank: 5, name: "Ash-KODES ", score: "2100.00" },
+  { Rank: 6, name: "Ishaan Gupta", score: "2100.00" },
+  { Rank: 7, name: "Mihirkumar Patel ", score: "1952.63" },
+  { Rank: 8, name: "Prakash A", score: "1900.00" },
+  { Rank: 9, name: "Ashish Pandagre", score: "1800.00" },
+  { Rank: 10, name: "Bharath Kumar kallepalli", score: "1661.33" },
+];
+export default Data;
