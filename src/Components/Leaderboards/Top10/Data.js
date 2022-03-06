14a52ae155b98463db71819977f72a96c0065658
akshatnema <20bcs022@iiitdmj.ac.in>
2022-03-06 21:03:44 +0530

    updated leaderboard
    Signed-off-by: akshatnema <20bcs022@iiitdmj.ac.in>

--- src/Components/Leaderboards/Top10/Data.js
+++ src/Components/Leaderboards/Top10/Data.js
@@ -3,11 +3,11 @@ const Data = [
   { Rank: 2, name: "Rishabh Raizada", score: "300.00" },
   { Rank: 3, name: "Ishaan Gupta", score: "250.00" },
   { Rank: 4, name: "Ash-KODES", score: "216.67" },
-  { Rank: 5, name: "Prakash A", score: "200.00" },
-  { Rank: 6, name: "Mihirkumar Patel", score: "200.00" },
-  { Rank: 7, name: "Ayush Kalia", score: "163.89" },
-  { Rank: 8, name: "Soham Chaudhary", score: "159.96" },
-  { Rank: 9, name: "Varun Chawla", score: "155.56" },
-  { Rank: 10, name: "Ashish Pandagree", score: "150.00" },
+  { Rank: 5, name: "Tarun Singh", score: "200.00" },
+  { Rank: 6, name: "Prakash A", score: "200.00" },
+  { Rank: 7, name: "Mihirkumar Patel", score: "200.00" },
+  { Rank: 8, name: "Ayush Kalia", score: "163.89" },
+  { Rank: 9, name: "Soham Chaudhary", score: "159.96" },
+  { Rank: 10, name: "Varun Chawla", score: "155.56" },
 ];
 export default Data;
